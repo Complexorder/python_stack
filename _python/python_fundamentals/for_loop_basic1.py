b7679517f8c910ccad5e5ad9b53f683a78cba064
Yazan Eleyan <Eleyan.yazan@gmail.com>
2023-11-17 16:03:42 +0200

    basic 1 assi

---
+++ _python/python_fundamentals/for_loop_basic1.py
@@ -0,0 +1,29 @@
+# for i in range(150):
+#     print(i)
+
+# for i in range (5,1000,1):
+#     if(i%5==0):
+#         print(i)
+
+# for i in range (1,100,1):
+#     if(i%10==0):
+#         print("CodingDojo")
+#     elif(i%5==0):
+#         print("coding")
+
+# sum=0
+# for i in range (1,500000,1):
+#     if(i%2==1):
+#         sum=sum+i 
+# print(sum)
+
+# for i in range (2018,0,-4):
+#     print(i)
+
+
+LowNum=2
+highNum=9
+mult=3
+for i in range(LowNum,highNum+1,1):
+    if(i%mult==0):
+        print(i)
